d897291b233e00ff713dcff9d05a39dadab6302f
Dolphin-glitch <114450467+Dolphin-glitch@users.noreply.github.com>
2023-08-05 20:48:15 -0400

    a

--- src/utils/fetch/requests.ts
+++ src/utils/fetch/requests.ts
@@ -2,7 +2,7 @@ import { deepmerge } from 'deepmerge-ts';
 import { AccessToken } from '@/utils/auth/server';
 import { Options } from './core';
 
-const bot_api_endpoint = process.env.NEXT_PUBLIC_API_ENDPOINT ?? 'https://api.dolphinnotbotdashboard.vercel.app';
+const bot_api_endpoint = process.env.NEXT_PUBLIC_API_ENDPOINT ?? 'http://localhost:8080';
 const discord_api_endpoint = 'https://discord.com/api/v9';
 
 export function botRequest<T extends Options>(session: AccessToken, options: T): T {
